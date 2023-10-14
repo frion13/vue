7271e841f7538a098af293e420d18afa13b1d0d1
frion13 <frio_13@bk.ru>
2023-10-15 00:59:04 +0300

    dz4

---
+++ vue-homework/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <ProductComponent></ProductComponent>
+
+ 
+
+ 
+  
+  
+  
+  
+</template>
+
+<script>
+import ProductComponent from './components/ProductComponent.vue'
+
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    ProductComponent,
+  
+
+   
+}
+}
+</script>
+
+<style lang="scss">
+#app {
+  display: flex;
+}
+</style>
---
+++ vue-homework/src/components/ProductComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+      <div v-for="(product, index) in products" :key="index" class="product-details">
+        <h2>{{ product.name }}</h2>
+        <p>{{ formattedPrice(product) }}</p>
+        <p>{{ product.available ? 'Available' : 'Out of stock' }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProductDetails',
+    data() {
+      return {
+        products: [
+          {
+            name: 'Product 1',
+            price: 99.99, 
+            available: true, 
+          },
+          {
+            name: 'Product 2',
+            price: 49.99, 
+            available: false, 
+          },
+          {
+            name: 'Product 3',
+            price: 100, 
+            available: true, 
+          },
+        ],
+      };
+    },
+    computed: {
+      formattedPrice() {
+        return (product) => {
+          return '$' + product.price.toFixed(2);
+        };
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .product-details {
+    border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    
+  }
+  </style>
+  
